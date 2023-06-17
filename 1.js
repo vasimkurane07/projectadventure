7b54062728df816ebe505d7be1965b22eae22edb
vasim kurane <kuranevasim@gmail.com>
2023-06-17 12:10:03 +0530

    project

---
+++ 1.js
@@ -0,0 +1,223 @@
+function fun1(){
+    console.log("hello");
+    let par=document.getElementById("filter");
+    let blank1="";
+    par.innerHTML=blank1;
+    let n=`<div class="row  justify-content-center  mb-4 " >
+    <div class="col-sm-12 col-md-9 ">
+  <div class="row ">
+    <div class=" card col-sm-4 col-md-6 card border-warning ">
+      <img src="https://www.dellaadventure.com/uploads/250747.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">1 Thriller Factor</h5>
+      <h1 class="card-text">VIRTUAL RACING CARS</h1>
+      <a href="#" class="btn btn-warning">View activity</a>
+    </div>
+    </div>
+    <div class=" card col-sm-4 col-md-6 card border-warning" >
+      <img src="https://www.dellaadventure.com/uploads/160048.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">1 Thriller Factor</h5>
+        <h1 class="card-text">VIRTUAL GAMES</h1>
+        <a href="#" class="btn btn-warning">View activity</a>
+      </div>
+    </div>
+  </div>
+    </div>
+  </div>  `;
+let sec=`<div class="row  justify-content-center mb-4" >
+<div class="col-sm-12 col-md-9 ">
+<div class="row ">
+<div class=" card col-sm-4 col-md-6 card border-warning ">
+  <img src="https://www.dellaadventure.com/uploads/5066chess-inside-img.jpg" class="card-img-top" alt="...">
+<div class="card-body">
+  <h5 class="card-title">1 Thriller Factor</h5>
+  <h1 class="card-text">CHESS</h1>
+  <a href="#" class="btn btn-warning">View activity</a>
+</div>
+</div>
+<div class=" card col-sm-4 col-md-6 card border-warning" >
+  <img src="https://www.dellaadventure.com/uploads/6839carrom-outside-img.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">1 Thriller Factor</h5>
+    <h1 class="card-text">CARROM</h1>
+    <a href="#" class="btn btn-warning">View activity</a>
+  </div>
+</div>
+</div>
+</div>
+</div>  `;
+
+let third=`<div class="row  justify-content-center mb-4 " >
+<div class="col-sm-12 col-md-9 ">
+<div class="row ">
+<div class=" card col-sm-4 col-md-6 card border-warning">
+  <img src="https://www.dellaadventure.com/uploads/8753shooting-range-outside-img.jpg" class="card-img-top" alt="...">
+<div class="card-body">
+  <h5 class="card-title">2 thriller factor</h5>
+  <h1 class="card-text">SHOOTING RANGE</h1>
+  <a href="#" class="btn btn-warning">View activity</a>
+</div>
+</div>
+<div class=" card col-sm-4 col-md-6 card border-warning" >
+  <img src="https://www.dellaadventure.com/uploads/8604Bungee-trampoline.png" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">2 thriller factor</h5>
+    <h1 class="card-text">BUNGEE TRAMPOLINE</h1>
+    <a href="#" class="btn btn-warning">View activity</a>
+  </div>
+</div>
+</div>
+</div>
+</div>`
+par.innerHTML=n+sec+third;
+}
+
+
+function fun2(){
+    let par=document.getElementById("filter");
+    let blank1="";
+    par.innerHTML=blank1;
+    let n=`<div class="row  justify-content-center mb-4 " >
+    <div class="col-sm-12 col-md-9 ">
+  <div class="row ">
+    <div class=" card col-sm-4 col-md-6 card border-warning ">
+      <img src="https://www.dellaadventure.com/uploads/2669rappelling-outside-img.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title text-danger">8 Thriller Factore</h5>
+      <h1 class="card-text">RAPPELING</h1>
+      <a href="#" class="btn btn-warning">View activity</a>
+    </div>
+    </div>
+    <div class=" card col-sm-4 col-md-6 card border-warning" >
+      <img src="https://www.dellaadventure.com/uploads/42859365ATV%20200cc.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title btn-warning"> 8 Thriller Factoree</h5>
+        <h1 class="card-text">ATV</h1>
+        <a href="#" class="btn btn-warning">View activity</a>
+      </div>
+    </div>
+  </div>
+    </div>
+  </div>`
+
+  let second=`<div class="row  justify-content-center mb-4 " >
+  <div class="col-sm-12 col-md-9 ">
+<div class="row ">
+  <div class=" card col-sm-4 col-md-6 card border-warning">
+    <img src="https://www.dellaadventure.com/uploads/8753shooting-range-outside-img.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">2 thriller factor</h5>
+    <h1 class="card-text">SHOOTING RANGE</h1>
+    <a href="#" class="btn btn-warning">View activity</a>
+  </div>
+  </div>
+  <div class=" card col-sm-4 col-md-6 card border-warning" >
+    <img src="https://www.dellaadventure.com/uploads/8604Bungee-trampoline.png" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">2 thriller factor</h5>
+      <h1 class="card-text">BUNGEE TRAMPOLINE</h1>
+      <a href="#" class="btn btn-warning">View activity</a>
+    </div>
+  </div>
+</div>
+  </div>
+</div>`
+let third=`<div class="row  justify-content-center mb-4 " >
+<div class="col-sm-12 col-md-9 ">
+<div class="row ">
+<div class=" card col-sm-4 col-md-6 card border-warning">
+  <img src="https://www.dellaadventure.com/uploads/8753shooting-range-outside-img.jpg" class="card-img-top" alt="...">
+<div class="card-body">
+  <h5 class="card-title">2 thriller factor</h5>
+  <h1 class="card-text">SHOOTING RANGE</h1>
+  <a href="#" class="btn btn-warning">View activity</a>
+</div>
+</div>
+<div class=" card col-sm-4 col-md-6 card border-warning" >
+  <img src="https://www.dellaadventure.com/uploads/8604Bungee-trampoline.png" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">2 thriller factor</h5>
+    <h1 class="card-text">BUNGEE TRAMPOLINE</h1>
+    <a href="#" class="btn btn-warning">View activity</a>
+  </div>
+</div>
+</div>
+</div>
+</div>`
+par.innerHTML=n+second+third;
+}
+
+function fun3()
+{
+    let par=document.getElementById("filter");
+    let blank1="";
+    par.innerHTML=blank1;
+    let n=`<div class="row  justify-content-center mb-4r " >
+    <div class="col-sm-12 col-md-9 ">
+  <div class="row">
+    <div class=" card col-sm-4 col-md-6 card border-warning ">
+      <img src="https://www.dellaadventure.com/uploads/8202rocket-ejector-outside-img.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">2 thriller factor</h5>
+      <h1 class="card-text">BUNGEE TRAMPOLINE</h1>
+      <a href="#" class="btn btn-warning">View activity</a>
+    </div>
+    </div>
+    <div class="card col-sm-4 col-md-6 card border-warning" >
+      <img src="https://www.dellaadventure.com/uploads/4499High%20Rope%20Challenge.JPG" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <h1 class="card-text">BUNGEE TRAMPOLINE</h1>
+        <a href="#" class="btn btn-warning">View activity</a>
+      </div>
+    </div>
+  </div>
+    </div>
+  </div>`
+  let second=`<div class="row  justify-content-center mb-4" >
+  <div class="col-sm-12 col-md-9 ">
+<div class="row ">
+  <div class=" card col-sm-4 col-md-6 card border-warning ">
+    <img src="https://www.dellaadventure.com/uploads/5066chess-inside-img.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">1 Thriller Factor</h5>
+    <h1 class="card-text">CHESS</h1>
+    <a href="#" class="btn btn-warning">View activity</a>
+  </div>
+  </div>
+  <div class=" card col-sm-4 col-md-6 card border-warning" >
+    <img src="https://www.dellaadventure.com/uploads/6839carrom-outside-img.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">1 Thriller Factor</h5>
+      <h1 class="card-text">CARROM</h1>
+      <a href="#" class="btn btn-warning">View activity</a>
+    </div>
+  </div>
+</div>
+  </div>
+</div>`
+let third=`<div class="row  justify-content-center mb-4r " >
+<div class="col-sm-12 col-md-9 ">
+<div class="row">
+<div class=" card col-sm-4 col-md-6 card border-warning ">
+  <img src="https://www.dellaadventure.com/uploads/8202rocket-ejector-outside-img.jpg" class="card-img-top" alt="...">
+<div class="card-body">
+  <h5 class="card-title">2 thriller factor</h5>
+  <h1 class="card-text">BUNGEE TRAMPOLINE</h1>
+  <a href="#" class="btn btn-warning">View activity</a>
+</div>
+</div>
+<div class="card col-sm-4 col-md-6 card border-warning" >
+  <img src="https://www.dellaadventure.com/uploads/4499High%20Rope%20Challenge.JPG" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <h1 class="card-text">BUNGEE TRAMPOLINE</h1>
+    <a href="#" class="btn btn-warning">View activity</a>
+  </div>
+</div>
+</div>
+</div>
+</div>`
+par.innerHTML=n+second+third;
+}
